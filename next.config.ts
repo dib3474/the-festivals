372a0585ba3764136a3576caa5748d95d0d083be
dib3474 <dib3474@gmail.com>
2025-12-08 10:56:10 +0900

    chore: 배포 버전 이미지 링크 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProduction ? "/the-festivals" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
