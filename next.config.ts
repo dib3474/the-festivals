546d31a013218b6cc8d1c42d4caca1dca4b5ba3b
dib3474 <dib3474@gmail.com>
2025-12-08 10:44:56 +0900

    chore: github page 정적 배포를 위한 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "tong.visitkorea.or.kr",
-        port: "",
-        pathname: "/cms/**",
-      },
       {
         protocol: "https",
         hostname: "tong.visitkorea.or.kr",
